0e64be6454993fb44e4ad62ad3e089a7812c14e0
Moon-Seng <moonseng.dev@gamil.com>
2022-04-25 10:19:17 +0630

    Finished For Desktop

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('gg');
+let email = document.querySelector('.email-input');
+let button = document.querySelector('.submit-input');
+let error = document.querySelector('.error-message')
+
+button.addEventListener('click' , function(){
+    if(email.value == ""){
+        error.classList.remove('hidden');
+    }
+    email.value = '';
+})
